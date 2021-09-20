5f100304592e9bcd6c1ae18dbbbd3255d42ce224
Валентин Тихонов <valentintihonov9@gmail.com>
2021-09-20 22:11:32 +0300

    Исправлена ошибка в игре brain-even

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -15,6 +15,7 @@ function isEvenNumber()
     line('Answer "yes" if the number is even, otherwise answer "no".');
     for ($i = 0, $j = 0; $i < 3 && $j === 0; $i++) {
         $randomNumber = rand(1, 100);
+        $question = $randomNumber;
         $answer = question($question);
         if (($randomNumber % 2) == 0) {
             $rightAnswer = 'yes';
