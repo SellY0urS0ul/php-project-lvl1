2905f9195e729f14a623609a931e7c556e837713
Валентин Тихонов <valentintihonov9@gmail.com>
2021-09-23 15:13:21 +0300

    Обновлен движок и игра "Калькулятор"

--- src/Engine.php
+++ src/Engine.php
@@ -36,26 +36,26 @@ function question(string $question): string
     $answer = prompt('Your answer');
     return $answer;
 }
-function game(array $dataArr, $stop, $name) 
+function game(array $dataArr, $stop, $name)
 {
     $count = 3;
     $counter = 0;
     while ($stop == 0 && $counter < $count) {
-    foreach ($dataArr as $value) {
-    $question = $value[0];
-    $rightAnswer = $value[1];
-    line("Question: %s", $question);
-    $answer = prompt('Your answer');
-    if ($answer == $rightAnswer) {
-        line('Correct!');
-    } else {
-        line("'{$answer}' is wrong answer ;(. Correct answer was '{$rightAnswer}'.");
-        line("Let's try again, %s!", $name);
-        $stop++;
-        break;
-    }
-    $counter++;
-    }
+        foreach ($dataArr as $value) {
+            $question = $value[0];
+            $rightAnswer = $value[1];
+            line("Question: %s", $question);
+            $answer = prompt('Your answer');
+            if ($answer == $rightAnswer) {
+                line('Correct!');
+            } else {
+                line("'{$answer}' is wrong answer ;(. Correct answer was '{$rightAnswer}'.");
+                line("Let's try again, %s!", $name);
+                $stop++;
+                break;
+            }
+            $counter++;
+        }
     }
     if ($stop == 0) {
         line("Congratulations, %s!", $name);
